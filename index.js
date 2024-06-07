2a7c0df8d60ab8c477ead0cedbbddc25e1e0787a
labesk <167761417+labesk@users.noreply.github.com>
2024-06-07 06:54:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let menuButton = document.getElementById("open-close-icon");
+let mobileMenu = document.getElementById("mobile-menu");
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open-menu"); 
+});
